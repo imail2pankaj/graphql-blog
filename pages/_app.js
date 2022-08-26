adfcfbe0e364580d3654523d2a6a9aa7679b61f1
pankaj <imail2pankaj@gmail.com>
2022-08-26 11:14:33 +0530

    Javascript Mastery graphql code

---
+++ pages/_app.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import '../styles/globals.scss';
+import { Layout } from '../components';
+import { DefaultSeo } from 'next-seo';
+
+function MyApp({ Component, pageProps }) {
+  return (
+
+    <Layout>
+      <DefaultSeo
+        title={undefined}
+        titleTemplate="%s | Data Protection Excellence (DPEX) Network"
+        defaultTitle="Data Protection Excellence (DPEX) Network"
+        description="The Data Protection Excellence (DPEX) Network, the first of its kind facility in the ASEAN region, aims to provide leadership, best practices, training, research and support for all things around data privacy from an operational perspective."
+        openGraph={{
+          images: [{
+            url: 'https://www.dpexnetwork.org/_next/image?url=%2Fimages%2Flogo.png&w=256&q=75'
+          }]
+        }}
+      />
+      <Component {...pageProps} />
+    </Layout>
+  );
+}
+
+export default MyApp;
